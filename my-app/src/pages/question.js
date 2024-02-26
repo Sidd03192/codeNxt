8f15d655480420b76e89ee208a92f17989e2895f
Stuistheman <Stuistheman@gmail.com>
2024-02-26 11:50:02 -0600

    .

--- my-app/src/pages/question.js
+++ my-app/src/pages/question.js
@@ -2,7 +2,7 @@ import React from 'react'
 export const QuestionPage=()=>{
 	return(
 		<div style={{width:'40%'}}>
-		<p1>HELLO THIS IS QUESTION</p1>
+		<p1>HELLO THIS IS QUESTION RAHHH</p1>
 		</div>
 	)
 }
