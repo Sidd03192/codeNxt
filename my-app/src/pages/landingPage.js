346101b78e53de763ce0281f6a517b422b0c178e
Stuistheman <Stuistheman@gmail.com>
2024-02-27 12:41:44 -0600

    Playing with cards >.<

--- my-app/src/pages/landingPage.js
+++ my-app/src/pages/landingPage.js
@@ -2,17 +2,33 @@ import React from 'react';
 import Compiler from '../Compiler/Complier';
 import {LandingCard} from './landingCards2';
 import {Spacer} from "@nextui-org/react";
-import {Card, CardHeader, CardBody, CardFooter, Divider, Link, Image} from "@nextui-org/react";
+import {Card, CardHeader, CardBody, CardFooter, Divider, Link, Image, Button} from "@nextui-org/react";
 
 export const LandingPage=()=> {
   return (
 
     <div className="flex">
-      <LandingCard />
-      <Spacer x={4} />
-      <LandingCard />
-      <LandingCard />
-      <Spacer x={4} />
+      
+       <Card
+      isFooterBlurred
+      radius="lg"
+      className="border-none"
+    >
+      <Image
+        alt="Woman listing to music"
+        className="object-cover"
+        height={200}
+        src="./logo.png"
+        width={200}
+      />
+      <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+        <p className="text-tiny text-white/80">Click here to get coding!</p>
+        <Button className="text-tiny text-black bg-black/20" variant="flat" color="default" radius="lg" size="sm">
+          Go!
+        </Button>
+      </CardFooter>
+    </Card>
+    <Spacer x = {4} />
       <LandingCard />
     </div>
  
