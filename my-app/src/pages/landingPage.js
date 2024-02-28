16c36bc50d6b655ef2405b6b976a4237ca6d79e3
Stuistheman <Stuistheman@gmail.com>
2024-02-28 12:41:10 -0600

    More landing page changes

--- my-app/src/pages/landingPage.js
+++ my-app/src/pages/landingPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Compiler from '../Compiler/Complier';
 import {LandingCard} from './landingCards2';
+import Calendar from 'react-calendar'
 import {Spacer} from "@nextui-org/react";
 import {Card, CardHeader, CardBody, CardFooter, Divider, Link, Image, Button} from "@nextui-org/react";
 
@@ -8,22 +9,24 @@ export const LandingPage=()=> {
   return (
 
     <div className="flex">
-      
+      <Spacer x = {2}/>
+      <Calendar/>
        <Card
       isFooterBlurred
       radius="lg"
       className="border-none"
     >
+   
       <Image
         alt="Woman listing to music"
         className="object-cover"
-        height={200}
-        src="./logo.png"
-        width={200}
+        height={300}
+        src={require("./codeNext.png")}
+        width={300}
       />
-      <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-        <p className="text-tiny text-white/80">Click here to get coding!</p>
-        <Button className="text-tiny text-black bg-black/20" variant="flat" color="default" radius="lg" size="sm">
+       <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+        <p className="tiny text-white/80">Click here to get coding! </p>
+        <Button className="tiny text-black bg-black/20" variant="outline" color="default" radius="lg" size="lg">
           Go!
         </Button>
       </CardFooter>
